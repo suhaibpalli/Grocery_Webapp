c67bdeaf53cd22dc20553f06a6a21d32705e71af
Bharath <bk3604073@gmail.com>
2023-08-01 22:50:20 +0530

    second commit

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -9,8 +10,13 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   public isAdmin = false
   token = false
+  cartList: any[] = []
 
-  constructor(private route: Router) {
+  constructor(private route: Router, private http:HttpClient,) {
+    const userId = localStorage.getItem('userId')
+    this.http.get<any[]>(`http://localhost:5100/cart/${userId}`).subscribe(data => {
+      this.cartList = data;
+    });
     const token = localStorage.getItem('jwtToken')
     if (token) {
       this.isAdmin = false
